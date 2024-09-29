32b294b19692a919ec6f3db8cc35dfaa600c58f3
musan6325 <musan1220191@outlook.jp>
2024-09-30 03:17:40 +0900

    lastlast

--- app/resources/views/todo/edit.blade.php
+++ app/resources/views/todo/edit.blade.php
@@ -7,7 +7,7 @@
         <div class="row">
             <div class="col-12 col-md-8 mx-auto">
                 {{-- バリデーションエラー部分テンプレート --}}
-                @include('layouts.errors')
+                
 
                 {{ Form::open(['url' => route('todo.update', $todo->id), 'method' => 'put']) }}
 
